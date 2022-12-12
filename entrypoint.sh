29eba93f8dc166889c0d26e6bbfaf1bc7df40c7d
ershan <ershan_coding@outlook.com>
2022-12-12 23:30:07 +0800

    Add adsense.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,6 @@ touch ${LOGSEQ_SRC}/${LOGSEQ_DEST}/.nojekyll
 # executing like "xvfb-run node publish.mjs -p graph -o www -t true --theme light"
 xvfb-run node publish.mjs -p ${LOGSEQ_SRC} \
     -o ${LOGSEQ_SRC}/${LOGSEQ_DEST} -t ${LOGSEQ_IS_TRACE} --theme ${LOGSEQ_THEME}
+
+replacement='<head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3247793043228755" crossorigin="anonymous"></script>'
+sed -i --expression "s@<head>@$replacement@" index.html
